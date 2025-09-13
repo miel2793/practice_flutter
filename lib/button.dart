977a1be0ba0bca5b272ed79ab966cc99233f9cb6
miel2793 <mahmudsifat2793@gmail.com>
2025-09-13 12:55:10 +0600

    add to github

--- lib/button.dart
+++ lib/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Button extends StatelessWidget {
   const Button({super.key});
 
@@ -6,21 +7,44 @@ class Button extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home",),
-        backgroundColor: Colors.deepOrange,
-
+        backgroundColor:Colors.black ,
+        title:
+        Text("Home Page ",style:TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 22,
+            color: Colors.deepOrange.shade600
+        ),),
       ),
-      body: Center(
+      body:Center(
+
         child: Column(
           children: [
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor:Colors.black,
-                    foregroundColor:Colors.deepOrange,
-                shape: RoundedRectangleBorder(borderRadius:20)
+                style: ElevatedButton.styleFrom(
+            backgroundColor:Colors.black,
+                  foregroundColor:Colors.deepOrange,
+                  shape:RoundedRectangleBorder(
+                    borderRadius:BorderRadius.circular(20)
+                  )
+        ),
+                onPressed:(){}, child: Text("Click Here")),
+            SizedBox(
+              height:15,
+            ),
+            SizedBox(
+              height: 50,
+              width: 120,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor:Colors.black,
+                      foregroundColor:Colors.deepOrange,
+                      shape:RoundedRectangleBorder(
+                          borderRadius:BorderRadius.circular(20)
+                      )
+                  ),
+                  onPressed:(){}, child: Text("Click Here")),
+            )
 
-              ),
-                onPressed:(){} , child:Text("Click Here"))
           ],
         ),
       ),
