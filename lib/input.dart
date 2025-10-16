bb4ab8d54d93652e9e3f0224437dc7e2a10cb6d9
miel2793 <mahmudsifat2793@gmail.com>
2025-10-16 16:58:14 +0600

    add to github

--- lib/input.dart
+++ lib/input.dart
@@ -5,7 +5,28 @@ class Input extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle:true,
+        backgroundColor: Colors.red,
+        title: Text(
+          'Input',style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
 
+
+
+        ),
+        ),
+      ),
+      body:Column(
+        children: [
+          TextField(
+
+          )
+
+        ],
+      ),
     );
   }
 }
