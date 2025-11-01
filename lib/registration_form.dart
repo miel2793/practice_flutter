ed129df11388446539f094be1954ad85e275d76d
miel2793 <mahmudsifat2793@gmail.com>
2025-11-01 11:13:30 +0600

    addd

--- lib/registration_form.dart
+++ lib/registration_form.dart
@@ -93,6 +93,7 @@ class Registration_form extends StatelessWidget {
                           {
                             return null;
                           }
+                        //
                       },
                     ),
                     SizedBox(
